57f8ac662eb1c7869d5d76f73ae5e9f452dc49e0
nBow <nbow51@gmail.com>
2018-11-13 20:31:35 -0800

    resolution fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.rippleDiv').ripples({
-        resolution: 900,
-        dropRadius: 10,
+        resolution: 512,
+        dropRadius: 20,
         perturbance: 0.04
       });
 
